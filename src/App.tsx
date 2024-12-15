9254603652560ef619c9ea7092edcc88c149919e
paulleadbeater1987 <paul_leadbeater@live.co.uk>
2024-12-15 17:11:44 +0000

    basic react structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Checkout System</h1>
+      <p>Welcome to the checkout system. Add items to your basket</p>
+    </div>
+  );
+}
+
+export default App;
